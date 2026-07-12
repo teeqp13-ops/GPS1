f50d6638af987bbf6b32644c4e9fb015d7f81ad0
teeqp13-ops <teeqp13@gmail.com>
2026-07-12 21:42:04 +0300

    Add safe configuration template

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+const APP_NAME = 'GPSPlus License';
+const DB_PATH = __DIR__ . '/storage/gpsplus.sqlite';
+const API_KEY = 'CHANGE_ME_TO_A_LONG_RANDOM_API_KEY';
+const ADMIN_USERNAME = 'admin';
+const ADMIN_PASSWORD = 'CHANGE_ME_TO_A_STRONG_PASSWORD';
+const SESSION_NAME = 'gpsplus_admin';
+date_default_timezone_set('Asia/Riyadh');
